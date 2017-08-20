76559b146e65f196b01b0ab4bd41408625b3db14
Tomasz Rakowski <akayami@gmail.com>
2017-08-20 10:25:23 -0400

    Many things fixed up. Reloading properly starts up and shuts down ports according to vhost configs.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const config = Object.freeze(require('plain-config')());
 const http = require('http');
 const https = require('https');
@@ -6,15 +7,22 @@ const fs = require('fs');
 const path = require('path');
 
 
+// Holds vhost instances
 let oStack = {
 	list: []
 }
 
-//var vhstack = [];
+// Holds all servers associed with their ports
+let serverList = {
 
-function load(oStack) {
+}
+
+function load(oStack, handler) {
 	console.info('Reloading vhosts');
-	var n = [];
+	let n = [];
+
+	let portMap = {};
+
 	fs.readdirSync(path.join(config.controller.vhost.path)).forEach(file => {
 		if (file.match(/\.vhost\.js$/)) {
 			let m = path.join(config.controller.vhost.path, file);
@@ -23,7 +31,12 @@ function load(oStack) {
 				delete require.cache[m];
 				let vh = require(m);
 				console.info('Loading vhost: ' + vh.name);
-				n.push(new vhost(vh));
+				let ovh = new vhost(vh);
+				if(portMap[ovh.config.vhost.port] && portMap[ovh.config.vhost.port] != ovh.config.vhost.protocol) {
+					throw new Error('Invalid configuration. Requested port already used by a different protocol: ' + ovh.config.vhost.port + ' mapped to ' + portMap[ovh.config.vhost.port])
+				}
+				portMap[ovh.config.vhost.port] = ovh.config.vhost.protocol;
+				n.push(ovh);
 			} catch(e) {
 				console.error('Failed to load: ' + m);
 				console.error(e);
@@ -33,24 +46,38 @@ function load(oStack) {
 	});
 	oStack.list = n;
 	console.info('All vhosts reloaded');
+
+	// Shutting down server ports that are not needed anymore;
+	console.info('Shutting down any unneeded ports');
+	for(let port in serverList) {
+		if(serverList[port] && !portMap[port]) {
+			console.info('Shutting down port:' + port);
+			serverList[port].close();
+			delete serverList[port];
+		}
+	}
+	// Adding any missing ports
+	console.info('Starting any additional ports');
+	for(let port in portMap) {
+		if(!serverList[port]) {
+			let protocol = portMap[port];
+			let s = require(protocol).createServer(handler).listen(port, (err) => {
+				console.info('Listening on port %d', s.address().port)
+			});
+			serverList[port] = s;
+			console.info('Started on port:' + port);
+		}
+	}
 }
 
 var handler = require('./lib/router.js')(oStack);
 
-var server = http.createServer(handler).listen(config.children.http.port, function() {
-	console.log('Listening on port %d', server.address().port);
-});
-
-var secure = https.createServer(config.children.https.options, handler).listen(config.children.https.port, function() {
-	console.log('Listening on port %d', secure.address().port);
-});
-
-load(oStack);
+load(oStack, handler);
 
 process.on('message', function(msg) {
 	switch (msg.action) {
 		case "reload":
-			load(oStack);
+			load(oStack, handler);
 			break;
 		default:
 			console.warn('Unknown inter-process message received: ' + msg.action);
---
+++ test/controller.js
@@ -0,0 +1,117 @@
+var c = require('../lib/controller');
+const server = require('http')
+const request = require('request')
+const fs = require('fs-extra');
+const path = require('path')
+
+var testRoot = '/tmp/hadbox-test'
+var confPath = path.join(testRoot, 'conf.d/');
+
+let socket = '/tmp/hardbox-unit'
+
+const Propagator = require('../lib/propagator');
+
+describe('Test controller', function() {
+
+	let stack;
+
+	const list = [];
+	const propagator = new Propagator(list);
+
+	beforeEach(function(done) {
+		fs.mkdirp(confPath, err => {
+			stack = c(propagator, {
+				endpoint: [{
+					server: require('http'),
+					listen: {
+						path: socket
+					}
+				}],
+				vhost: {
+					path: confPath
+				}
+			}, function(err, r) {
+				done(err);
+			});
+		})
+	})
+
+	afterEach(function(done) {
+		stack.close(function(err) {
+			if(err) {
+				return done(err);
+			}
+			fs.remove(testRoot, function(err) {
+				return done(err);
+			})
+		})
+	})
+
+	// it('Needs to startup with a path endpoint', function(done) {
+	// 	request.put('http://unix:/tmp/hardbox:/vhost/set/vhost/key.value.sdsd/value',function(error, response, body) {
+	// 		if (error) {
+	// 			done(error);
+	// 		} else {
+	// 			if (response.statusCode == 200) {
+	// 				done();
+	// 			} else {
+	// 				done(response.statusCode)
+	// 			}
+	// 		}
+	// 	});
+	// })
+
+	it('Needs to reload', function(done) {
+		fs.writeFileSync(path.join(confPath, 'file1.js'), `
+		module.exports = {
+
+			name: 'test',
+
+			vhost: {
+				protocol: 'https',
+				domain: 'test.dev',
+				port: '8043',
+				pathname: '/'
+			},
+			target: "http://localhost:3000",
+
+			morgan: {
+				format: 'tiny',
+				options: {}
+			}
+		}
+		`);
+
+		fs.writeFileSync(path.join(confPath, 'file2.js'), `
+		module.exports = {
+
+			name: 'test',
+
+			vhost: {
+				protocol: 'https',
+				domain: 'test.dev',
+				port: '8043',
+				pathname: '/'
+			},
+			target: "http://localhost:3000",
+
+			morgan: {
+				format: 'tiny',
+				options: {}
+			}
+		}
+		`);
+
+		request.get(`http://unix:${socket}:/reload`,function(error, response, body) {
+			if (error) {
+				done(error);
+			} else {
+				if (response.statusCode == 200) {
+					done();
+				} else {
+					done(response.statusCode)
+				}
+			}
+		});
+	})
+})
