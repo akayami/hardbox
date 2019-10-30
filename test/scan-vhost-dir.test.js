2c33bdf0df9331a84984f73b43673be08448af8f
Tomasz Rakowski <tomasz.rakowski@indexexchange.com>
2019-10-30 10:38:04 -0400

    Improved test.

--- test/scan-vhost-dir.test.js
+++ test/scan-vhost-dir.test.js
@@ -1,8 +1,13 @@
 const func = require('../lib/scan-vhost-dir');
 const path = __dirname + '/vhost/';
+const {should, expect} = require('chai');
 
 describe('Test the scan-vhost-dir function', () => {
 	it('Needs to load contents', () => {
 		const o = func(path);
+		expect(o).to.be.an('array').of.length(1);
+		expect(o[0]).to.be.an('object');
+		expect(o[0].name).to.be.a('string').equal('test');
+		expect(o[0].vhost).to.be.an('object');
 	});
 });
